e5660c729ee010a90857f089b2f2b85eda6a36da
cowrrr <costly-chant.00@icloud.com>
2025-05-29 02:15:59 +0300

    proper cmd args

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
-
+#include <math.h>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb/stb_image_write.h"
 
@@ -16,30 +17,79 @@ int width, height, channels;
 long int FileSize(const char* input);
 void WriteImage(int compr, unsigned char *input);
 char output_file[11] = "temp.jpg";
+char *output_name = NULL;
+char *input_file = NULL;
+int verbose;
+int replace = 1;
+double target_size;
+char *endptr;
+int max_attempts = 10;
+float range = 10;
 
 int main(int argc, char *argv[]) {
-    if (argc < 6) {
-        fprintf(stderr, "Usage: %s <input_file> <target_size_in_bytes> <max_attempts> <range in %%> <replace>\n", argv[0]);
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s <input_file> <target_size_in_megabytes> <max_attempts: opt> <range in %%: opt> <replace: opt>\n", argv[0]);
         return 1;
     }
 
-    char *endptr;
-    int replace = strtol(argv[5], &endptr, 10);
-    long int target_size = strtol(argv[2], &endptr, 10);
-    float range = strtol(argv[4], &endptr, 10);
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "-verbose") == 0) {
+            verbose = 1;
+        }
+        else if (strcmp(argv[i], "-i") == 0 || strcmp(argv[i], "-input") == 0) {
+            if (i + 1 < argc) {
+                input_file = argv[i + 1];
+                i++;
+            } else {
+                fprintf(stderr, "Error: -i or --input requires a filename.\n");
+                return 1;
+            }
+        } else if (strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "-output") == 0) {
+            if (i + 1 < argc) {
+                output_name = argv[i + 1];
+                replace = 0;
+                i++;
+            } else {
+                fprintf(stderr, "Error: -o or --output requires a filename.\n");
+                return 1;
+            }
+        } else if (strcmp(argv[i], "-t") == 0 || strcmp(argv[i], "-target") == 0) {
+            if (i + 1 < argc) {
+                target_size = strtol(argv[i + 1], &endptr, 10);
+                target_size = target_size * 1024 * 1024;
+            } else {
+               fprintf(stderr, "Error: -t or -target requires a size.\n");
+               return 1; 
+            }
+        } else if (strcmp(argv[i], "-ma") == 0) {
+            if (i + 1 < argc) {
+                max_attempts = strtol(argv[i + 1], &endptr, 10);
+            } else {
+                fprintf(stderr, "Error: -ma requires a value\n");
+            }
+        } else if (strcmp(argv[i], "-r") == 0) {
+            if (i + i < argc) {
+                range = strtol(argv[i + 1], &endptr, 10);
+            } else {
+                fprintf(stderr, "Error: -r requires a value\n");
+            }
+        }
+
+    }
+
+
+    printf("%f\n", target_size);
     int best_low = 0;
     int best_high = 100;
     int middle = 100;
     double tenprtarget = (target_size * (1 - (range / 100)));
     int attempts = 1;
-    int max_attempts = strtol(argv[3], &endptr, 10);
 
     if (*endptr != '\0' || target_size <= 0) {
-        fprintf(stderr, "Invalid target size. Please provide a positive number of bytes.\n");
+        fprintf(stderr, "Invalid target size. Please provide a positive number of megabytes\n");
         return 1;
     }
 
-    char *input_file = argv[1];
     
     unsigned char *input = stbi_load(input_file, &width, &height, &channels, 0);
     if (!input) {
